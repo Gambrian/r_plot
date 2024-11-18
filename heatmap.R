9de03376886c83fed5c2fbaabdb9a0ecda2dd330
GENG <614700682@qq.com>
2024-11-18 19:08:19 +0800

    add:singleCell plot

---
+++ heatmap.R
@@ -0,0 +1,16 @@
+
+options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
+
+
+
+# https://mp.weixin.qq.com/s?__biz=MzIyNDI1MzgzOQ==&mid=2650401013&idx=1&sn=7bd4fd1719bf6235a47d7892ab1bdcfa&scene=21#wechat_redirect
+
+install.packages("pheatmap")
+devtools::install_github("xmc811/Scillus", ref = "development")
+
+library(tidyverse)
+library(readxl)
+library(stringi)
+
+
+
